7187d790d8767a10cd867142c1e64d2196cf02df
Satishgundlapally <89225850+Satishgundlapally@users.noreply.github.com>
2021-11-12 10:08:58 -0600

    updated

--- controllers/Elephant.js
+++ controllers/Elephant.js
@@ -6,9 +6,17 @@ exports.Elephant_list = function(req, res) {
 }; 
  
 // for a specific Elephant. 
-exports.Elephant_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Elephant detail: ' + req.params.id); 
+exports.Elephant_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Elephant.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
+ 
  
 // Handle Elephant create on POST. 
 exports.Elephant_create_post = function(req, res) { 
@@ -54,7 +62,7 @@ exports.Elephant_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"Elephant_type":"goat", "cost":12, "size":"large"} 
+    // {"Elephant_colour":"goat", " age":12, "weight":"large"} 
     document.colour = req.body.colour; 
     document.age = req.body.age; 
     document.weight = req.body.weight; 
@@ -66,4 +74,24 @@ exports.Elephant_create_post = async function(req, res) {
         res.status(500); 
         res.send(`{"error": ${err}}`); 
     }   
+}; 
+
+exports.Elephant_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Elephant.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.colour)  
+               toUpdate.colour = req.body.colour; 
+        if(req.body. age) toUpdate. age = req.body. age; 
+        if(req.body.weight) toUpdate.weight = req.body.weight; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
